8569224f999a8f0c023d06f1dc65902f3f2e42b3
myeong chul <10jade0070@gmail.com>
2018-12-27 10:00:47 +0900

    Player
    Vive Input Virtual Button

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void OnVirtualPress()
+    {
+        Debug.Log("OnVirtualPress");
+    }
+
+    public void OnVirtualClick()
+    {
+        Debug.Log("OnVirtualClick()");
+    }
+
+    public void OnVirtualPressDown()
+    {
+        Debug.Log("OnVirtualPressDown()");
+    }
+
+    public void OnVirtualPressUp()
+    {
+        Debug.Log("OnVirtualPressUp()");
+    }
+}
